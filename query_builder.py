d331c264f52462f71d8de3a9a547e87a06299d8b
prp0x80 <prashant.patel@gmx.com>
2025-01-10 18:10:56 +0000

    Minor updates for presentation

--- query_builder.py
+++ query_builder.py
@@ -1,5 +1,14 @@
-from data import query_semantic_layer_data
-from run_sql import query_bigquery
+#   SELECT
+#     [ WITH differential_privacy_clause ]
+#     [ { ALL | DISTINCT } ]
+#     [ AS { STRUCT | VALUE } ]
+#     select_list
+#   [ FROM from_clause[, ...] ]
+#   [ WHERE bool_expression ]
+#   [ GROUP BY group_by_specification ]
+#   [ HAVING bool_expression ]
+#   [ QUALIFY bool_expression ]
+#   [ WINDOW window_clause ]
 
 SELECT = "SELECT "
 FROM = "FROM "
@@ -8,14 +17,7 @@ GROUP_BY = "GROUP BY "
 HAVING = "HAVING "
 
 
-def get_all_tables_and_columns(metrics: list[dict], dimensions: list[dict]) -> tuple[list[str], list[str]]:
-    projections = metrics + dimensions
-    tables = list({x["table"] for x in projections})
-    columns = list({f'{x["table"]}.{x["sql"]}' for x in dimensions})
-    return tables, columns
-
-
-def process_semantics(query_semantic_layer: tuple[dict, dict]) -> dict[str, list]:
+def preprocess_query_semantic_data(query_semantic_layer: tuple[dict, dict]) -> dict[str, list]:
     query = query_semantic_layer[0]
     semantic_layer = query_semantic_layer[1]
     filters = query.get("filters")
@@ -37,6 +39,13 @@ def process_semantics(query_semantic_layer: tuple[dict, dict]) -> dict[str, list
         data["joins"] = joins
 
     return data
+
+
+def get_all_tables_and_columns(metrics: list[dict], dimensions: list[dict]) -> tuple[list[str], list[str]]:
+    projections = metrics + dimensions
+    tables = list({x["table"] for x in projections})
+    columns = list({f'{x["table"]}.{x["sql"]}' for x in dimensions})
+    return tables, columns
 
 
 def right_strip(input_string: str, string_to_be_removed: str) -> str:
@@ -63,18 +72,22 @@ def build_select(metrics: list[dict], dimensions: list[dict]) -> str:
     return SELECT + right_strip(m, ", ")
 
 
-def build_from_using_join(joins: list[dict]) -> str:
-    j = ""
-    for join in joins:
-        j += f'{join["one"]} JOIN {join["many"]} ON {join["join"]}\n'
-    return FROM + j.strip()
+def build_from(joins: list[dict], tables: list[str]) -> str:
+    if len(tables) == 1:
+        from_stmt = FROM + tables[0]
+    else:
+        j = ""
+        for join in joins:
+            j += f'{join["one"]} JOIN {join["many"]} ON {join["join"]}\n'
+        from_stmt = FROM + j.strip()
+    return from_stmt
 
 
 def build_groupby(columns: list[str]) -> str:
     return GROUP_BY + ", ".join(columns)
 
 
-def build_where(filters: list[dict], columns: list[dict]) -> str:
+def build_where(filters: list[dict], columns: list[str]) -> str:
     where_stmt = ""
     columns = [c.split(".")[1] for c in columns]
     for filter in filters:
@@ -89,7 +102,7 @@ def build_where(filters: list[dict], columns: list[dict]) -> str:
     return where_stmt
 
 
-def build_having(filters: list[dict], columns: list[dict]) -> str:
+def build_having(filters: list[dict], columns: list[str]) -> str:
     having_stmt = ""
     columns = [c.split(".")[1] for c in columns]
     for filter in filters:
@@ -104,56 +117,84 @@ def build_having(filters: list[dict], columns: list[dict]) -> str:
     return having_stmt
 
 
-def has_metric_filter(filters: list[dict], columns: list[dict]) -> bool:
+def has_metric_filter(filters: list[dict], columns: list[str]) -> bool:
     filter_fields = [x["field"] for x in filters]
     column_names = [x.split(".")[1] for x in columns]
     return any([f not in column_names for f in filter_fields])
 
+if __name__ == "__main__":
+    from pprint import pprint
+    from data import query_semantic_layer_data
+    from run_sql import query_bigquery
 
-for idx, semantic in enumerate(query_semantic_layer_data, 1):
-    data = process_semantics(semantic)
-    metrics = data.get("metrics")
-    dimensions = data.get("dimensions", [])
-    filters = data.get("filters", [])
-    joins = data.get("joins", [])
-    tables, columns = get_all_tables_and_columns(metrics, dimensions)
-    select_stmt = build_select(metrics, dimensions)
-    # print(select_stmt)
+    # iterate over the query semantic layer data
+    for idx, semantic in enumerate(query_semantic_layer_data, 1):
 
-    if len(tables) == 1:
-        from_stmt = FROM + tables[0]
-    else:
-        from_stmt = build_from_using_join(joins)
-    # print(from_stmt)
+        # preprocess the data
+        data = preprocess_query_semantic_data(semantic)
 
-    where_stmt = ""
-    if filters:
-        if not has_metric_filter(filters, columns):
-            where_stmt = build_where(filters, columns)
-            # print(where_stmt)
+        # get the metrics, dimensions, filters and joins
+        metrics = data.get("metrics")
+        dimensions = data.get("dimensions", [])
+        filters = data.get("filters", [])
+        joins = data.get("joins", [])
 
-    group_by_stmt = ""
-    if len(columns) > 0:
-        group_by_stmt = build_groupby(columns)
-        # print(group_by_stmt)
+        # get all tables and columns
+        tables, columns = get_all_tables_and_columns(metrics, dimensions)
 
-    having_stmt = ""
-    if filters:
-        if has_metric_filter(filters, columns):
-            having_stmt = build_having(filters, columns)
-            # print(having_stmt)
+        # build the SQL query
+        select_stmt = build_select(metrics, dimensions)
+        # print(select_stmt)
 
-    sql_stmt = []
-    sql_stmt.append(select_stmt)
-    sql_stmt.append(from_stmt)
-    if where_stmt:
-        sql_stmt.append(where_stmt)
-    if group_by_stmt:
-        sql_stmt.append(group_by_stmt)
-    if having_stmt:
-        sql_stmt.append(having_stmt)
+        from_stmt = build_from(joins, tables)
+        # print(from_stmt)
 
-    print(" ".join(sql_stmt))
-    print()
-    query_bigquery(" ".join(sql_stmt))
-    print()
+        # if the filter is on non-metric columns, we need a where statement
+        where_stmt = ""
+        if filters:
+            if not has_metric_filter(filters, columns):
+                where_stmt = build_where(filters, columns)
+                # print(where_stmt)
+
+        # since metric is always present
+        # a group by statement is needed if there are other columns in addition to the metrics
+        group_by_stmt = ""
+        if len(columns) > 0:
+            group_by_stmt = build_groupby(columns)
+            # print(group_by_stmt)
+
+        # if the filter is on a metric column, we need a having statement
+        having_stmt = ""
+        if filters:
+            if has_metric_filter(filters, columns):
+                having_stmt = build_having(filters, columns)
+                # print(having_stmt)
+
+        # combine all the statements
+        sql_stmt = []
+        sql_stmt.append(select_stmt)
+        sql_stmt.append(from_stmt)
+        if where_stmt:
+            sql_stmt.append(where_stmt)
+        if group_by_stmt:
+            sql_stmt.append(group_by_stmt)
+        if having_stmt:
+            sql_stmt.append(having_stmt)
+
+
+        print(f"Query#{idx}")
+
+        # print the data
+        pprint(data)
+        print()
+        
+        # print the SQL statement
+        print(f'SQL Query: {" ".join(sql_stmt)}')
+        print()
+
+        # run the SQL statement
+        print("Results:")
+        query_bigquery(" ".join(sql_stmt))
+        print()
+        print("="*100)
+
